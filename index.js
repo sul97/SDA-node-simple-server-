d22580d95cb7b22277fe3fcbc3c95bcc395ed961
sarojniraula <sarojniraula@outlook.com>
2023-08-31 11:16:49 +0300

    Updated index file

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const PORT = '8080'
+
+http
+  .createServer((req, res) => {
+    /* handle http requests */
+  })
+  .listen(PORT);
+
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
